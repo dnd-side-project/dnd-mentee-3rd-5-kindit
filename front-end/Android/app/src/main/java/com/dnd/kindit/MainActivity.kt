27f6a738f1be16483e4c69dc02de992cb3bb31f5
JangJaeWoo <wkdwoduf333@naver.com>
2020-08-01 01:18:17 +0900

    화면 하단 바텀 네비게이션 추가
    * main / search / recommend / config 패키지 파일 디렉토리 나누고 각각 프레그먼트 추가
    
    * gradle 바텀 네비게이션 도입을 위한 머티리얼 디자인 라이브러리 추가
    
    * 프레그먼트 기본 화면 xml 파일 추가
    
    * res / menu 디렉토리 생성 후 바텀 네비게이션 설정파일 추가

--- front-end/Android/app/src/main/java/com/dnd/kindit/MainActivity.kt
+++ front-end/Android/app/src/main/java/com/dnd/kindit/MainActivity.kt
@@ -2,10 +2,39 @@ package com.dnd.kindit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dnd.kindit.config.ConfigFragment
+import com.dnd.kindit.main.MainFragment
+import com.dnd.kindit.recommend.RecommendFragment
+import com.dnd.kindit.search.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //첫화면 지정
+        MainFragment().let {
+            supportFragmentManager.beginTransaction().add(R.id.contentContainer, it).commitAllowingStateLoss()
+        }
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+
+            val mainFragment = MainFragment()
+            val searchFragment = SearchFragment()
+            val recommendFragment = RecommendFragment()
+            val configFragment = ConfigFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+
+            when(item.itemId){
+                R.id.mainItem -> transaction.replace(R.id.contentContainer,mainFragment).commitAllowingStateLoss()
+                R.id.searchItem -> transaction.replace(R.id.contentContainer, searchFragment).commitAllowingStateLoss()
+                R.id.recommendItem -> transaction.replace(R.id.contentContainer, recommendFragment).commitAllowingStateLoss()
+                R.id.configItem -> transaction.replace(R.id.contentContainer, configFragment).commitAllowingStateLoss()
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
+
     }
 }
---
+++ front-end/Android/app/src/main/java/com/dnd/kindit/main/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.dnd.kindit.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dnd.kindit.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_main,container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
