4120a91161108762fb9dbfaaf48af5b1b303fba7
msnodeve <msnodeve@gmail.com>
2020-08-02 00:30:44 +0900

    feature: 로딩창 구현

---
+++ front-end/Android/app/src/main/java/com/dnd/kindit/view/LauncherActivity.kt
@@ -0,0 +1,32 @@
+package com.dnd.kindit.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dnd.kindit.MainActivity
+import com.dnd.kindit.R
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+        
+        // 로딩중 가져올 데이터 가져올 기능
+//        startLoading()
+
+        // 임시 로딩 시간
+        Handler().postDelayed({
+            // 추후 LoginActivity로 이동
+//            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, 200)
+    }
+    private fun startLoading(){
+        
+    }
+
+}
