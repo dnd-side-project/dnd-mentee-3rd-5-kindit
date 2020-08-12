0d666f1769dc999506a7d79b550a17c65cc8efed
msnodeve <msnodeve@gmail.com>
2020-08-12 21:51:47 +0900

    feature: Service Login Activity View 구현

--- front-end/Android/app/src/main/java/com/dnd/kindit/retrofit/service/AccountService.kt
+++ front-end/Android/app/src/main/java/com/dnd/kindit/retrofit/service/AccountService.kt
@@ -1,5 +1,12 @@
 package com.dnd.kindit.retrofit.service
 
-interface AccountService{
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface AccountService{
+    @GET("api/accounts/auth/check-nickname/")
+    fun checkUsableUserNickname(
+        @Query("nickname") nickname: String
+    ): Call<String>
 }
