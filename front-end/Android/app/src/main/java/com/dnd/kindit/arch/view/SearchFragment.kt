f3ceb3cb949ede91798873ac4579acc82740a485
msnodeve <msnodeve@gmail.com>
2020-09-04 01:41:18 +0900

    feature: Search Page 데이터 목록 받아오기 구현 완료 - 검색 기능 추가해야함

--- front-end/Android/app/src/main/java/com/dnd/kindit/arch/view/SearchFragment.kt
+++ front-end/Android/app/src/main/java/com/dnd/kindit/arch/view/SearchFragment.kt
@@ -8,16 +8,21 @@ import android.view.ViewGroup
 import android.widget.GridLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.dnd.kindit.R
 import com.dnd.kindit.arch.adapter.SearchAdapter
 import com.dnd.kindit.arch.model.SearchItem
 import com.dnd.kindit.arch.viewmodel.SearchViewModel
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
+
     private val TAG = this.javaClass.toString()
 
+    private lateinit var layout: GridLayoutManager
     private lateinit var list: ArrayList<SearchItem>
     private lateinit var searchViewModel : SearchViewModel
 
@@ -28,14 +33,38 @@ class SearchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val layout = GridLayoutManager(context, 2)
+        init()
+
+        initViewModelFun()
+
+        getData()
+
+        initListener()
+
+    }
+
+    private fun init() {
+        layout = GridLayoutManager(context, 2)
         fs_rcv_items.layoutManager = layout
 
-        list = ArrayList()
-        for(i in 0..5){
-            list.add(SearchItem("$i", i+0L, "$i", i+0L, ""))
-        }
-        fs_rcv_items.adapter = SearchAdapter(list)
+        searchViewModel = ViewModelProviders.of(this.activity!!).get(SearchViewModel::class.java)
 
+        searchViewModel.getMenuList(this.context!!)
+    }
+
+    private fun initViewModelFun() {
+        searchViewModel.searchItemList.observe(this, Observer {
+            list = ArrayList()
+            for(item in it.data){
+                list.add(SearchItem(name=item.title, viewCount = item.hits, userName = item.writer, starCount = item.rating, imgPic = item.uploadImage))
+            }
+            fs_rcv_items.adapter = SearchAdapter(list)
+        })
+    }
+
+    private fun getData() {
+    }
+
+    private fun initListener() {
     }
 }
